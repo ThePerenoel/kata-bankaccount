f95e23b40970e457f8107461102161464e6459a1
ThePerenoel <loicnoel12@gmail.com>
2018-09-06 15:45:12 +0200

    Deposit is called several times with different amounts and adds it to its Transactions

--- src/test/java/test/BankAccountTest.java
+++ src/test/java/test/BankAccountTest.java
@@ -12,17 +12,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BankAccountTest {
 
     @Nested
-    @DisplayName("Deposit method should")
+    @DisplayName("deposit method should")
     class DepositTest {
 
         @Test
-        void should_add_20_to_its_transactions_when_given_20() {
+        void add_20_to_its_transactions_when_given_20() {
             Transactions bankAccountTransactions = new TestTransactions();
             BankAccount bankAccount = new BankAccount(bankAccountTransactions);
             Transactions testTransactions = new TestTransactions();
 
             bankAccount.deposit(20);
             testTransactions.add(20);
+
+            assertThat(bankAccountTransactions).isEqualTo(testTransactions);
+        }
+
+        @Test
+        void add_20_and_30_to_its_transactions_when_given_20_and_30() {
+            Transactions bankAccountTransactions = new TestTransactions();
+            BankAccount bankAccount = new BankAccount(bankAccountTransactions);
+            Transactions testTransactions = new TestTransactions();
+
+            bankAccount.deposit(20);
+            bankAccount.deposit(30);
+            testTransactions.add(20);
+            testTransactions.add(30);
 
             assertThat(bankAccountTransactions).isEqualTo(testTransactions);
         }
